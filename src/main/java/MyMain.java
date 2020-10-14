99a306a473a62d297a0ef1e37c59cb26d29d906a
Kiran-O <22kokelly@athenian.org>
2020-10-14 16:36:56 +0000

    Passed all tests

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 public class MyMain {
 
     public static int someMethod() {
-        return 1;
+        
+
+        return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = (int) (11 * Math.random() + 10);
+        int b = (int) (11 * Math.random() + 10);
+        int c = (int) (11 * Math.random() + 10);
+        int qwe = Math.max(a, Math.max(b,c));
+        int ynk = Math.min(a, Math.min(b,c));
+        String d = ""+a;
+        String e = ""+b;
+        String f = ""+c;
+        String tou = ""+qwe;
+        String jkl = ""+ynk;
+        
+        System.out.println("The three integers are: "+d+e+f);
+        System.out.println("The largest integer is: "+tou);
+        System.out.println("The smallest integer is: "+jkl);
     }
 
 }
