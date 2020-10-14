9505a84423b5caf7cad97d484213b1b71e1cdb32
Kiran-O <22kokelly@athenian.org>
2020-10-14 17:09:24 +0000

    Passed All tests. Mathey completed too.

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -1,3 +1,45 @@
 public class Mathey {
+    public static int max(int x, int y){
+        if (x > y){
+            return x;
+
+        }
+        else {
+            return y;
+        }
+    }
+
+    public static double max(double x, double y){
+        if (x > y){
+            return x;
+
+        }
+        else {
+            return y;
+        }
+    }
+
+    public static int max(int x, int y, int z){
+        return max(x, max(y, z));
+    }
+
+    public static double max(double x, double y, double z){
+        return max(x ,max(y, z));
+    }
+
+    public static int max(int x, int y, int z, int q){
+        int qwe = max(x ,max(y, z));
+        return max(qwe, q);
+    }
+
+    public static int randomInteger(int a, int b){
+        return (int) (Math.random()*(b-a+1))+a;
+
+    }
+
+    public static int randomInteger(int b){
+        return (int) (Math.random()*(b+1));
+
+    }
 
 }
